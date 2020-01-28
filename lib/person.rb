f2e48b050fdd937b55c006b36db0b599c1a59d1e
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-28 20:59:58 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  
+  def name=(p_name)
+    @name = p_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(work)
+    @job = work
+  end
+  
+  def job
+    @job
+  end
+  
+end
+
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+beyonce.job = 
+puts beyonce.name
